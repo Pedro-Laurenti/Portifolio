b6428a8131d988e268614b808a6c65df0e0a26f7
Pedro Laurenti <pedrolaurenti04@gmail.com>
2023-05-16 22:28:39 -0300

    ignore

--- custom.js
+++ custom.js
@@ -36,3 +36,32 @@ function typeWrite(elemento, tempoPausa, tempoEscrita, tempoApagamento){
 
 const titulo = document.querySelector('.titulo-card-animado');
 typeWrite(titulo, 4000, 75, 50);
+
+//animação no scrool
+
+function smoothScroll(targetId) {
+    const targetElement = document.getElementById(targetId);
+    const targetOffset = targetElement.offsetTop;
+    const scrollDuration = 1000; // Duração da animação em milissegundos
+    const scrollStep = Math.PI / (scrollDuration / 15); // Valor do incremento de rolagem
+
+    let scrollCount = 0;
+    let currPos = window.pageYOffset;
+
+    const scrollInterval = setInterval(() => {
+    if (scrollCount >= scrollDuration) {
+        clearInterval(scrollInterval);
+    }
+
+    scrollCount += 15;
+    const newPos = easeInOut(currPos, targetOffset, scrollCount, scrollDuration);
+    window.scrollTo(0, newPos);
+    }, 15);
+}
+
+function easeInOut(start, end, current, duration) {
+    const delta = end - start;
+    const currentTime = current / duration;
+    const easeInCubic = currentTime * currentTime * currentTime;
+    return start + delta * easeInCubic;
+}
